5dd1ec758c62a50b797ce231ef51e250cc13c6dd
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-01-02 18:28:26 +0300

    while commit

--- learningPython/While.py
+++ learningPython/While.py
@@ -2,3 +2,26 @@ i=5
 while i<=10:
     i += 1
     print(i)
+
+a = 20
+while a <= 30:
+    a +=1
+    if a%5 ==0 : continue
+    print(a)
+else:
+    print ("a değeri 30'dan küçük değil")
+
+    print("Ders notlarınızın ortalamasını hesapla.")
+    print("Not girişi bittiğinde boşluk giriniz.")
+    ortalama = 0
+    toplam = 0
+    notsayisi = 0
+    dersnotu = ""
+
+while dersnotu != " ":
+        dersnotu = input("Ortalaması alınacak notu giriniz:")
+        if dersnotu == " ": break
+        toplam += int(dersnotu)
+        notsayisi += 1
+ortalama = toplam / notsayisi
+print("Ortalamanız:", ortalama)
